50d57fcb13664e8372a56e11655f888ece1dfed7
ZabDom <zablotny@student.agh.edu.pl>
2019-06-02 13:57:08 +0200

    ParksManager updated

--- ParkingSystem/EjbSlotImpl/src/main/java/pl/agh/soa/ejb/services/impl/SlotManagerBean.java
+++ ParkingSystem/EjbSlotImpl/src/main/java/pl/agh/soa/ejb/services/impl/SlotManagerBean.java
@@ -1,17 +1,20 @@
 package pl.agh.soa.ejb.services.impl;
 
+import pl.agh.soa.dao.ParkingSlotDAO;
 import pl.agh.soa.dto.ParkingSlotData;
+import pl.agh.soa.dto.ParksData;
 import pl.agh.soa.ejb.services.local.SlotManagerLocal;
 import pl.agh.soa.ejb.services.remote.SlotManagerRemote;
 
-import javax.ejb.Local;
-import javax.ejb.Remote;
-import javax.ejb.Stateless;
+import javax.ejb.*;
+import java.sql.DatabaseMetaData;
+import java.util.Date;
 
 @Remote(SlotManagerRemote.class)
 @Local(SlotManagerLocal.class)
 @Stateless
-public class SlotManagerBean implements SlotManagerLocal, SlotManagerRemote {
+public class SlotManagerBean implements SlotManagerLocal, SlotManagerRemote
+{
     @Override
     public String testest() {
         return "asdasd";
