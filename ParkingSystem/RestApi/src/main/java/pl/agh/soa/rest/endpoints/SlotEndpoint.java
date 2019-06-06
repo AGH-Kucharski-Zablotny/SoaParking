cd2c49c0a35f6dd8120fee084b17d49224c290ff
ZabDom <zablotny@student.agh.edu.pl>
2019-06-06 11:57:16 +0200

    Slot State

---
+++ ParkingSystem/RestApi/src/main/java/pl/agh/soa/rest/endpoints/SlotEndpoint.java
@@ -0,0 +1,23 @@
+package pl.agh.soa.rest.endpoints;
+
+import pl.agh.soa.ejb.services.remote.SlotManagerRemote;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("/miejsca")
+public class SlotEndpoint
+{
+    @EJB(lookup = "java:global/EjbSlotImpl-1.0/SlotManagerBean!pl.agh.soa.ejb.services.remote.SlotManagerRemote")
+    private SlotManagerRemote slotManager;
+
+    @GET
+    @Produces("application/json")
+    @Path("/{id}")
+    public Response getSlot(@PathParam("id") int slotId)
+    {
+        // response status ok
+        return Response.status(200).entity(slotManager.getSlot(slotId)).build();
+    }
+}
